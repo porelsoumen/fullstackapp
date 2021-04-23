d35411b85fc77c310e8651755c66cb35db567159
Soumen Porel <soumen.porel@lithium.com>
2021-04-23 17:05:18 +0530

    Exercise 2.2

--- part2/courseinfo/src/Course.js
+++ part2/courseinfo/src/Course.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './Header'
 import Content from './Content'
+import Total from './Total'
 
 const Course = (props) => {
 
@@ -8,6 +9,7 @@ const Course = (props) => {
     <div>
     <Header header={props.course.name}/>
     <Content parts={props.course.parts} />
+    <Total parts={props.course.parts} />
     </div>
   )
 }
--- part2/courseinfo/src/Total.js
+++ part2/courseinfo/src/Total.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const Total = (props) => {
+	let sum = 0;
+	props.parts.map(part => sum += part.exercises)
 	return (
 		<div>
-		<p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+		<p>Total of {sum} exercises</p>
 		</div>
 	)
 }
