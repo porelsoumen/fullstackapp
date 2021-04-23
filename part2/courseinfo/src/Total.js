2606da7434481afcf80477e4a8e1873b5814ddf1
Soumen Porel <soumen.porel@lithium.com>
2021-04-23 17:12:14 +0530

    Exercise 2.3

--- part2/courseinfo/src/Total.js
+++ part2/courseinfo/src/Total.js
@@ -1,11 +1,10 @@
 import React from 'react'
 
 const Total = (props) => {
-	let sum = 0;
-	props.parts.map(part => sum += part.exercises)
+	
 	return (
 		<div>
-		<p>Total of {sum} exercises</p>
+		<p>Total of {props.parts.reduce((accumulator, currentItem) => accumulator + currentItem.exercises,0)} exercises</p>
 		</div>
 	)
 }
