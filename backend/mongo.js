89bb0f9a099907ae41a1d94ca5a72f8c7286c276
Soumen Porel <soumen.porel@lithium.com>
2021-07-13 20:54:20 +0530

    Exercise 3.12

---
+++ backend/mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.gi8ov.mongodb.net/phonebookDB?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const phonebookSchema = new mongoose.Schema({
+    id: Number,
+  name: String,
+  number: String,
+})
+
+const Contact = mongoose.model('Contact', phonebookSchema)
+
+if (process.argv.length > 3) {
+    const contact = new Contact({
+        id: 1,
+        name: name,
+          number: number
+      })
+      
+      contact.save().then(result => {
+        console.log(`Added ${name} number ${number} to phonebook`)
+        mongoose.connection.close()
+      })
+} else {
+    Contact.find({}).then(result => {
+        result.forEach(contact => {
+            console.log(contact)
+        })
+        mongoose.connection.close()
+    })
+}
