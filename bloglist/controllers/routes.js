247899a326a846745913eb39ba0dd0f9168fbe8d
Soumen Porel <soumen.porel@lithium.com>
2021-08-17 23:20:29 +0530

    Exercise 4.19

--- bloglist/controllers/routes.js
+++ bloglist/controllers/routes.js
@@ -1,9 +1,17 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
+function getTokenFrom(request) {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+        return authorization.substring(7)
+    }
+    return null
+}
 blogRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({}).populate('user')
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1})
     response.json(blogs)
 })
   
@@ -12,17 +20,23 @@ blogRouter.post('/', async (request, response) => {
     if (!body.title && !body.url) {
         return response.status(400).json({ error: 'Missing title & URL'})
     }
-    const users = await User.find({})
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+        return response.status(400).json({error: 'token missing or invalid'})
+    }
+
+    const user = await User.findById(decodedToken.id)
 
     if (!body.likes) {
         body.likes = 0
     }
-    body.user = users[0]._id
+    body.user = user._id
     const blog = new Blog(body)
   
     const result = await blog.save()
-    users[0].blogs = users[0].blogs.concat(result._id)
-    await users[0].save()
+    user.blogs = user.blogs.concat(result._id)
+    await user.save()
     response.status(201).json(result)
 })
 
