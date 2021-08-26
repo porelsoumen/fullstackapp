c3124706edaeb43d4bef526b3474d72c8965b881
Soumen Porel <soumen.porel@lithium.com>
2021-08-26 14:14:46 +0530

    Exercise 4.6

--- bloglist/utils/list_helper.js
+++ bloglist/utils/list_helper.js
@@ -6,8 +6,30 @@ const totalLikes = (blogs) => {
     }, 0)
 }
 
+const mostBlogs = (blogs) => {
+    const authors = blogs.map(blog => blog.author)
+    let uniqAuthors = []
+    for (let a of authors) {
+        if (!uniqAuthors.includes(a)) {
+            uniqAuthors.push(a)
+        }
+    }
+    let authorsBlogs = []
+    for (let a of uniqAuthors) {
+        const blogCnt = blogs.filter(blog => blog.author === a).length
+        const obj = {
+            author: a,
+            blogs: blogCnt 
+        }
+        authorsBlogs.push(obj)
+    }
+    const maxblogs = Math.max.apply(Math, authorsBlogs.map(o => o.blogs))
+    const maxAuthor = authorsBlogs.find(author => author.blogs === maxblogs)
+    return maxAuthor
+}
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    mostBlogs
 }
 
